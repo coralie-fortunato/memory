b23607be80e284f2caf12848c6aa926b1565f74e
celine-emptoz-lacote <62133744+celine-emptoz-lacote@users.noreply.github.com>
2020-07-21 10:20:38 +0200

    Creation de la classe DataBase

--- classes/database.php
+++ classes/database.php
@@ -5,8 +5,30 @@ class DataBase {
     private $host;
     private $utilisateur;
     private $mdp;
+    private $name;
+    private $bd;
 
-    
+    public function __construct($host,$utilisateur,$mdp,$name){
+        $this->host = $host;
+        $this->utilisateur = $utilisateur;
+        $this->mdp = $mdp;
+        $this->name = $name;
+    }
+
+    public function connect() {
+        try {
+            $this->bd = new PDO("mysql:host=$this->host;dbname=$this->name", $this->utilisateur, $this->mdp);
+            return $this->bd;
+        }
+        catch (PDOException $e) {
+            print "Erreur !: " . $e->getMessage() . "<br/>";
+        
+        }
+    }
+
+    public function disconnect() {
+        $his->bd->close();
+    }
 }
 
 
