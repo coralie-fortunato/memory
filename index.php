a4a23e55026b0ab0aef8c3cd119d0d73de5e0386
celine-emptoz-lacote <62133744+celine-emptoz-lacote@users.noreply.github.com>
2020-07-22 15:23:53 +0200

    Creation de la page

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="src/fontello/css/fontello.css">
+    <title>Document</title>
+</head>
+<body>
+    <header><?php include 'includes/header.php'; ?></header>
+
+    <main></main>
+
+    <footer><?php include 'includes/footer.php'; ?></footer>
+</body>
+</html>
